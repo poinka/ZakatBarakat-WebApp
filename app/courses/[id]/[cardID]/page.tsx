009e2087d35aabd3d1868a6f1af823ecea384e04
poinka <pkorobejnikova18@gmail.com>
2024-07-14 17:58:32 +0300

    added card body

--- app/courses/[id]/[cardID]/page.tsx
+++ app/courses/[id]/[cardID]/page.tsx
@@ -1,3 +1,6 @@
+import {supabase } from "@/lib/supabase";
+import Card from "@/app/types"
+
 type Props = {
     params: {
       cardID: number;
@@ -6,7 +9,17 @@ type Props = {
   
   
 export default async function CardPage({ params: { cardID } }: Props) {
+  const { data: cards } = await supabase.from("cards").select().eq("id", cardID);
+  if (!cards || cards.length === 0) {
+    // Handle the case where no card is found for the ID
+    return <div>Information not found.</div>;
+  }
+  const card = cards[0] as Card;
+
     return (
+      <>
         <h1>Card {cardID}</h1>
+        <p>{card.body}</p>
+        </>
     )
 }
