760e37daa99eff76316a5f930464dc8e20574da4
poinka <pkorobejnikova18@gmail.com>
2024-07-16 23:12:57 +0300

    Added recommended course

--- components/RecommendedCourse.tsx
+++ components/RecommendedCourse.tsx
@@ -4,7 +4,7 @@ import CourseCard from "./CourseCard";
 export default function RecommendedCourse(course: Course) {
     return (
       <div className='flex-row flex p-6 pt-16 justify-center items-center'> 
-        <div className="w-full lg:w-52 m-0">
+        <div className="lg:w-52 m-0 w-40">
           <CourseCard {...course} />
         </div>
         <div className="flex flex-col items-start text-start text-green-800 text-sm lg:text-xl p-5 w-40">
