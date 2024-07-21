7ae63dce19529a6b8103b587ee004f355db89928
poinka <pkorobejnikova18@gmail.com>
2024-07-21 17:55:52 +0300

    Fix: heights of items

--- components/ArticleCard.tsx
+++ components/ArticleCard.tsx
@@ -5,8 +5,8 @@ import { Card, CardContent } from "@/components/ui/card";
 const ArticleCard: React.FC<Article> = ({ title, description, imageUrl, id }) => {
   return (
     <Link className='hover:scale-105' href={`/articles/${id}`} style={{ textDecoration: "none" }}>
-      <Card className="w-11/12 h-52 m-5 shadow-xl rounded-lg overflow-hidden bg-white p-6 relative z-10 border border-gray-200" style={{borderRadius: "15px"}}>
-        <CardContent className="">
+      <Card className="w-11/12 md:h-52 h-72 m-5 shadow-xl rounded-lg overflow-hidden bg-white p-6 border border-gray-200" style={{borderRadius: "15px"}}>
+        <CardContent className="p-0">
           <h2 className="text-lg font-bold mb-4 text-green-900">{title}</h2>
           <p className="" style={{color: "#9EAC9E"}}>{description}</p>
         </CardContent>
